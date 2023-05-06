ae216878472eab6ab939d6154a4f550519477f94
hakanaktas0 <104701041+hakanaktas0@users.noreply.github.com>
2023-05-06 22:53:57 +0300

    Secret Key is omitted
    A different key will be used in deployment.

--- practice_app/practice_app/settings.py
+++ practice_app/practice_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b+g%8zg=kxk79bbpj2527obyccp)7hl2y19bh8df)hjb-t1y_("
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
