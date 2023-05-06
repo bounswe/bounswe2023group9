27893c81a6ab8d9a1dfde0eba4aba5c1361ce4a2
hakanaktas0 <104701041+hakanaktas0@users.noreply.github.com>
2023-05-06 23:02:42 +0300

    tests are enhanced

--- practice_app/api/tests.py
+++ practice_app/api/tests.py
@@ -3,7 +3,7 @@ from unittest import skip
 import requests
 from . import api_keys
 # Create your tests here.
-class API_GET_test_cases(TestCase):
+class google_scholar_test_cases(TestCase):
     def setUp(self):
         self.c = Client()
 
@@ -11,8 +11,16 @@ class API_GET_test_cases(TestCase):
     def tearDown(self):
         print('GET Tests Completed Successfully')
 
+    def test_404_responses(self):
+        self.assertEquals(self.c.get("/api/serp-api/?title=").status_code, 404)
+        self.assertEquals(self.c.get("/api/serp-api/?").status_code, 404)
+        self.assertEquals(self.c.get("/api/serp-api/").status_code, 404)
+        self.assertEquals(self.c.get("/api/serp-api/?rows=5").status_code, 404)
+        self.assertEquals(self.c.get("/api/serp-api/title=sad").status_code, 404)
+        self.assertEquals(self.c.get("/api/serp-api/?title=&").status_code, 404)
+
     @skip('limited use for this api')
-    def test_google_scholar(self):
+    def test_results(self):
         serp_api_response = requests.get('https://serpapi.com/search.json?engine=google_scholar&q=test&hl=en&num=3&api_key=' + api_keys.api_keys['serp_api'])
         self.assertEquals(serp_api_response.status_code,200,"SerpApi didn't work as supposed to")
         serp_api_response = serp_api_response.json()['organic_results']
@@ -35,12 +43,8 @@ class API_GET_test_cases(TestCase):
             self.assertEquals(serp_api_response[count]['link'], result['url'])
             self.assertEquals(serp_api_response[count]['position'], result['pos'])
             count += 1
-        self.assertEquals(self.c.get("/api/serp-api/?title=").status_code,404)
-        self.assertEquals(self.c.get("/api/serp-api/?").status_code, 404)
-        self.assertEquals(self.c.get("/api/serp-api/").status_code, 404)
-        self.assertEquals(self.c.get("/api/serp-api/?rows=5").status_code, 404)
-        self.assertEquals(self.c.get("/api/serp-api/title=sad").status_code, 404)
-        self.assertEquals(self.c.get("/api/serp-api/?title=&").status_code, 404)
+
+
 
 
 
