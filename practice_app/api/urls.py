de5656f41aa3d02ed909edc2ccba4155c56dc3cf
Ahmed Bera Pay <69896238+Bera0422@users.noreply.github.com>
2023-05-06 16:41:33 +0300

    add get method implementation and relevant tests

--- practice_app/api/urls.py
+++ practice_app/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
-]
+app_name = "api"
+urlpatterns = [
+    path('eric/', views.eric_index, name='eric_index'),
+    path('eric/papers/', views.eric_papers, name='eric_papers'),
+    
+    ]
