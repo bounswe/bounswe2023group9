66a3f84c42ef8671ab4c189df00c33b00767a22f
Ahmed Bera Pay <abera.pay@gmail.com>
2023-05-07 15:15:19 +0300

    Update urls.py

--- practice_app/practice_app/urls.py
+++ practice_app/practice_app/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("api/", include("api.urls")),
+    path("api/",include("api.urls")),
     path("admin/", admin.site.urls),
 ]
