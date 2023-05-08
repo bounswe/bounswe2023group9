bb0da6d3d5ee94d25ce89fb0a486029d228721af
Ali Mert Geben <79109830+gebenalimert@users.noreply.github.com>
2023-05-08 03:20:37 +0300

    Update views.py

--- practice_app/api/views.py
+++ practice_app/api/views.py
@@ -109,7 +109,7 @@ def eric_papers(request):
         return JsonResponse({'message':'Internal server error'}, status=503)
 def zenodo(request):
     ACCESS_TOKEN = api_keys.api_keys['zenodo_api']
-    search_title = request.GET.get("search_title", None)
+    search_title = request.GET.get("title", None)
     rows = request.GET.get('rows', 5)
     if search_title is None or search_title == "" or search_title.isspace() is True:
         return JsonResponse({'status': 'Title to search must be given.'}, status=404)
