b52f18a5b7bd8302b9f00a923088e440677028ef
defabdullah <ahmet.susuz@boun.edu.tr>
2023-05-09 16:55:28 +0300

    fixed typo

--- practice_app/api/urls.py
+++ practice_app/api/urls.py
@@ -5,5 +5,5 @@ app_name = "api"
 urlpatterns = [
     path("google-scholar/", views.google_scholar, name="google-scholar"),
     path('eric/', views.eric_papers, name='eric_papers'),
-    path('semantic-scholar/', views.semantic_scholar, name='semantic-scholarc')
+    path('semantic-scholar/', views.semantic_scholar, name='semantic-scholar')
     ]
