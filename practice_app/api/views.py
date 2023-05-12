ff16ff19a69ca92575098798a683506ea8013425
Ali Mert Geben <79109830+gebenalimert@users.noreply.github.com>
2023-05-12 07:11:22 +0300

    Fixed an unrelated bug

--- practice_app/api/views.py
+++ practice_app/api/views.py
@@ -282,7 +282,7 @@ def zenodo(request):
             paper_info['source'] = 'Zenodo'
             authors = paper['metadata']['creators']
             for author in authors:
-                paper_info['authors'].append(author['name'])
+                paper_info['authors'].append({'name':author['name']})   
             results.append(paper_info.copy()) #Add the paper attributes into results
         response['results'] = results 
         return JsonResponse(response, status=200) #Return results as response
