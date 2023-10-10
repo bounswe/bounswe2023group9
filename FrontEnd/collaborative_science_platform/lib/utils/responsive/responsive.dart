d6e16e97427c8be1bbc555aec0a82eb5469de742
Omer <omerfaunal@outlook.com>
2023-10-10 22:09:57 +0300

    Add initial folder structure and some necessary beginning functions

---
+++ FrontEnd/collaborative_science_platform/lib/utils/responsive/responsive.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+const double tabletBreakpoint = 768;
+const double desktopBreakpoint = 1200;
+
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget desktop;
+
+  const Responsive({
+    Key? key,
+    required this.mobile,
+    this.tablet,
+    required this.desktop,
+  }) : super(key: key);
+
+  static bool isMobile(BuildContext context) => MediaQuery.of(context).size.width < tabletBreakpoint;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width >= tabletBreakpoint && MediaQuery.of(context).size.width < desktopBreakpoint;
+
+  static bool isDesktop(BuildContext context) => MediaQuery.of(context).size.width >= desktopBreakpoint;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth >= desktopBreakpoint) {
+          return desktop;
+        } else if (constraints.maxWidth >= tabletBreakpoint && tablet != null) {
+          return tablet!;
+        } else {
+          return mobile;
+        }
+      },
+    );
+  }
+}
