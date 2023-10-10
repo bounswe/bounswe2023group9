6ef099ef45121ab11e3d00feb2f7ac428d17a54c
Omer <omerfaunal@outlook.com>
2023-10-10 21:50:08 +0300

    Create flutter project

---
+++ FrontEnd/collaborative_science_platform/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:collaborative_science_platform/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Constants.appName,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
