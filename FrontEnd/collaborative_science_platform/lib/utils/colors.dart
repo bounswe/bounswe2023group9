75428f66d82089022c89fe15a8828fa4d8eede6a
Omer <omerfaunal@outlook.com>
2023-10-10 21:54:04 +0300

    Initial file structure

---
+++ FrontEnd/collaborative_science_platform/lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF007AFF);
+  static const Color secondaryColor = Color(0xFF6C757D);
+  static const Color tertiaryColor = Color(0xFFE5E5E5);
+
+  static const Color successColor = Color(0xFF28A745);
+  static const Color warningColor = Color(0xFFFFC107);
+  static const Color dangerColor = Color(0xFFDC3545);
+  static const Color infoColor = Color(0xFF17A2B8);
+}
