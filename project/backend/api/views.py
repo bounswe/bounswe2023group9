00e7143f3b01c59a22043245ba12e8f8267f97b8
hakanaktas0 <104701041+hakanaktas0@users.noreply.github.com>
2023-10-23 01:10:56 +0300

    search implementation

--- project/backend/api/views.py
+++ project/backend/api/views.py
@@ -6,6 +6,15 @@ from database.serializers import UserSerializer, RegisterSerializer
 from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import generics
+from django.http import JsonResponse, HttpRequest
+from django.contrib.postgres.search import SearchVector
+from database import models
+
+# from nltk.corpus import wordnet as wn
+# import nltk
+#
+# nltk.download('wordnet')
+
 
 # Create your views here.
 
@@ -24,4 +33,86 @@ class UserDetailAPI(APIView):
     serializer = UserSerializer(user)
 
     return Response(serializer.data)
-    
+
+
+
+
+
+def get_proof_from_id(request):
+    id = int(request.GET.get("proof_id"))
+    proof = models.Theorem.objects.filter(theorem_id=id)
+    return JsonResponse({'proof_id': proof[0].proof_id,
+                         'proof_title': proof[0].proof_title,
+                         'proof_content': proof[0].proof_content,
+                         'is_valid': proof[0].is_valid,
+                         'is_disproof': proof[0].is_disproof,
+                         'publish_date': proof[0].publish_date,
+                         }, status=200)
+
+
+
+def get_theorem_from_id(request):
+    id = int(request.GET.get("theorem_id"))
+    theorem = models.Theorem.objects.filter(theorem_id=id)
+    return JsonResponse({'theorem_id': theorem[0].theorem_id,
+                         'theorem_title': theorem[0].theorem_title,
+                         'theorem_content': theorem[0].theorem_content,
+                         'publish_date': theorem[0].publish_date,
+                         }, status=200)
+
+
+def get_node_from_id(request):
+    id = int(request.GET.get("node_id"))
+    node = models.Node.objects.filter(node_id=id)
+    return JsonResponse({'node_title':node[0].node_title,
+                         'theorem': node[0].theorem.theorem_id,
+                         'publish_date': node[0].publish_date,
+                         'is_valid':node[0].is_valid,
+                         'annotations' : node[0].annotations,
+                         'wiki_tags':node[0].wiki_tags,
+                         'num_visits':node[0].num_visits,
+                         'semantic_tags':node[0].semantic_tags,
+                         'reviewers':node[0].reviewers,
+                         'contributors':node[0].contributors,
+                         }, status=200) # REFERENCE NODES MISSING
+
+def search(request):
+    search = request.GET.get("query")
+    search_type = request.GET.get("type")
+    if search == None or search == "":
+        return JsonResponse({'status': 'Title to search must be given.'}, status=400)
+    if search_type == None or search_type == "":
+        return JsonResponse({'status': 'Type to search must be given.'}, status=400)
+    search_elements = search.split()
+    # similars = [] # TODO ADVANCED SEARCH
+    # also_sees = []
+    #
+    # for element in search_elements:
+    #     if  len(wn.synsets(element)) != 0:
+    #         for el in wn.synsets(element)[0].also_sees():
+    #             el = el.name()
+    #             el = el[:el.find('.')]
+    #             also_sees.append(el)
+    #         for el in wn.synsets(element)[0].similar_tos():
+    #             el = el.name()
+    #             el = el[:el.find('.')]
+    #             similars.append(el)
+    # start search with exact search
+    nodes = []
+    contributors = []
+    if search_type == 'node' or search_type == 'both':
+        for el in search_elements:
+            res = models.Node.objects.annotate(search=SearchVector("node_title")).filter(node_title__icontains=el)
+            for e in res:
+                nodes.append(e.node_id)
+    if search_type == 'author' or search_type == 'both':
+        for el in search_elements:
+            res_name = models.User.objects.filter(first_name__icontains=el)
+            res_surname = models.User.objects.filter(last_name__icontains=el)
+            for e in res_name:
+                if models.Contributor.objects.filter(user_id=e.id).count() != 0:
+                    contributors.append(e.username)
+            for e in res_surname:
+                if models.Contributor.objects.filter(user_id=e.id).count() != 0:
+                    contributors.append(e.username)
+    return JsonResponse({'nodes' : nodes , 'authors' : list(set(contributors))},status=200)
