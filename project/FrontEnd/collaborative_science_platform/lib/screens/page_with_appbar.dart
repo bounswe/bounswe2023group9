5d20393d6ce06aeb97a8582af1bd288539d3bcf7
Omer <omerfaunal@outlook.com>
2023-10-22 21:44:22 +0300

    Add documentation

--- project/FrontEnd/collaborative_science_platform/lib/screens/page_with_appbar.dart
+++ project/FrontEnd/collaborative_science_platform/lib/screens/page_with_appbar.dart
@@ -1,11 +1,25 @@
 import 'package:collaborative_science_platform/utils/responsive/responsive.dart';
 import 'package:flutter/material.dart';
 
+/// A widget for creating a page with a customizable app bar and content.
+///
+/// This widget allows you to create a page with an app bar and content area.
+/// The app bar can be customized using the [appBar] parameter, and the content
+/// can be set using the [child] parameter. You can also specify the background
+/// color of the content area using the [pageColor] parameter and control
+/// whether the content is scrollable with the [isScrollable] parameter.
 class PageWithAppBar extends StatelessWidget {
   final Widget child;
   final Widget appBar;
   final Color pageColor;
   final bool isScrollable;
+
+  /// Creates a [PageWithAppBar] widget.
+  ///
+  /// The [child] parameter represents the content to be displayed below the app bar.
+  /// The [appBar] parameter is a widget that serves as the app bar.
+  /// The [pageColor] parameter specifies the background color of the content area (default: Colors.white).
+  /// The [isScrollable] parameter indicates whether the content is scrollable (default: true).
   const PageWithAppBar(
       {required this.child, required this.appBar, this.pageColor = Colors.white, this.isScrollable = true, super.key});
 
