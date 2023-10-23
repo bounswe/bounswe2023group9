b23295b37fb925725da9bdfbe50f4669c2a9ca3c
hakanaktas0 <104701041+hakanaktas0@users.noreply.github.com>
2023-10-24 01:40:50 +0300

    Update tests.py

--- project/backend/api/tests.py
+++ project/backend/api/tests.py
@@ -86,9 +86,10 @@ class ProfileGETAPITestCase(TestCase):
         node.contributors.add(cont)
         self.get_profile_url = reverse('get_profile')
     def tearDown(self):
-        Contributor.objects.all().delete()
-        User.objects.all().delete()
         Node.objects.all().delete()
+        # Contributor.objects.all().delete()
+        User.objects.all().delete()
+
 
     def test_get_user_profile(self):
         data = {'mail':'test@example.com'}
