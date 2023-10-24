e813d283b6583e47df36ac53e6075158263a7112
Omer <omerfaunal@outlook.com>
2023-10-24 15:30:29 +0300

    Add please login screen

--- project/FrontEnd/collaborative_science_platform/lib/screens/profile_page/profile_options.dart
+++ project/FrontEnd/collaborative_science_platform/lib/screens/profile_page/profile_options.dart
@@ -1,8 +1,11 @@
 import 'package:collaborative_science_platform/models/user.dart';
 import 'package:collaborative_science_platform/providers/auth.dart';
+import 'package:collaborative_science_platform/screens/auth_screens/login_page.dart';
+import 'package:collaborative_science_platform/screens/auth_screens/signup_page.dart';
 import 'package:collaborative_science_platform/screens/home_page/home_page_appbar.dart';
 import 'package:collaborative_science_platform/screens/page_with_appbar.dart';
 import 'package:collaborative_science_platform/screens/profile_page/account_settings.dart';
+import 'package:collaborative_science_platform/utils/colors.dart';
 import 'package:collaborative_science_platform/utils/responsive/responsive.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,12 +18,12 @@ class ProfileOptions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final User user = Provider.of<Auth>(context).user!;
+    final User? user = Provider.of<Auth>(context).user;
     return PageWithAppBar(
       appBar: const HomePageAppBar(),
       child: Responsive(
-        mobile: MobileProfileOptions(user: user),
-        desktop: DesktopProfileOptions(user: user),
+        mobile: user != null ? MobileProfileOptions(user: user) : const PleaseLoginPage(),
+        desktop: user != null ? DesktopProfileOptions(user: user) : const PleaseLoginPage(),
       ),
     );
   }
@@ -281,6 +284,75 @@ class MobileProfileOptions extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class PleaseLoginPage extends StatelessWidget {
+  const PleaseLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: Responsive.getGenericPageWidth(context),
+      child: Column(
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+            child: Divider(height: 40.0),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              const Text("To be able to see your profile, please login!"),
+              const SizedBox(height: 5),
+              MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: GestureDetector(
+                  onTap: () => Navigator.pushNamed(context, LoginPage.routeName),
+                  child: Container(
+                    height: 40.0,
+                    width: 160,
+                    decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(5.0)),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Login',
+                            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16.0)),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text("If you don't have an account please signup!"),
+              const SizedBox(height: 5),
+              MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: GestureDetector(
+                  onTap: () => Navigator.pushNamed(context, SignUpPage.routeName),
+                  child: Container(
+                    height: 40.0,
+                    width: 160,
+                    decoration: BoxDecoration(color: AppColors.primaryColor, borderRadius: BorderRadius.circular(5.0)),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Signup',
+                            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16.0)),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+            child: Divider(height: 40.0),
+          ),
+        ],
       ),
     );
   }
