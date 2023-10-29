daf1b17b1fd64e8f5f0fe05f2fbd60e7eef9e396
defabdullah <ahmet.susuz@boun.edu.tr>
2023-10-30 01:53:23 +0300

    search types changed (forget prev commit.)

--- project/FrontEnd/collaborative_science_platform/lib/widgets/app_search_bar.dart
+++ project/FrontEnd/collaborative_science_platform/lib/widgets/app_search_bar.dart
@@ -1,19 +1,18 @@
 import 'package:collaborative_science_platform/utils/responsive/responsive.dart';
 import 'package:flutter/material.dart';
 
-enum SearchType {theorem, user, author }
+enum SearchType { theorem, author, by, both }
 
 class AppSearchBar extends StatefulWidget {
   final Function(SearchType) onSearch;
   final FocusNode focusNode;
   final TextEditingController controller;
 
-  const AppSearchBar({
-    required this.onSearch,
-    required this.controller,
-    required this.focusNode,
-    super.key
-  });
+  const AppSearchBar(
+      {required this.onSearch,
+      required this.controller,
+      required this.focusNode,
+      super.key});
 
   @override
   State<AppSearchBar> createState() => _AppSearchBarState();
@@ -25,25 +24,37 @@ class _AppSearchBarState extends State<AppSearchBar> {
   Widget searchTypeSelector() {
     if (Responsive.isMobile(context)) {
       return Icon(
-        (searchType == SearchType.theorem) ? Icons.description_rounded
-        : (searchType == SearchType.user) ? Icons.person
-        : Icons.science_rounded,
+        (searchType == SearchType.theorem)
+            ? Icons.description_rounded
+            : (searchType == SearchType.author)
+                ? Icons.person
+                : (searchType == SearchType.by)
+                    ? Icons.person_2_outlined
+                    : Icons.list_rounded,
         color: Colors.indigo.shade500,
       );
     } else {
       return Row(
         children: [
           Icon(
-            (searchType == SearchType.theorem) ? Icons.description_rounded
-            : (searchType == SearchType.user) ? Icons.person
-            : Icons.science_rounded,
+            (searchType == SearchType.theorem)
+                ? Icons.description_rounded
+                : (searchType == SearchType.author)
+                    ? Icons.person
+                    : (searchType == SearchType.by)
+                        ? Icons.person_2_outlined
+                        : Icons.list_rounded,
             color: Colors.indigo.shade500,
           ),
           const SizedBox(width: 4.0),
           Text(
-            (searchType == SearchType.theorem) ? "Theorem"
-            : (searchType == SearchType.user) ? "User"
-            : "Author",
+            (searchType == SearchType.theorem)
+                ? "Theorem"
+                : (searchType == SearchType.author)
+                    ? "Author"
+                    : (searchType == SearchType.by)
+                        ? "By"
+                        : "Both",
             style: TextStyle(
               color: Colors.indigo.shade500,
               fontWeight: FontWeight.w500,
@@ -68,7 +79,9 @@ class _AppSearchBarState extends State<AppSearchBar> {
           MouseRegion(
             cursor: SystemMouseCursors.click,
             child: GestureDetector(
-              onTap: widget.onSearch(searchType),
+              onTap: () {
+                widget.onSearch(searchType);
+              },
               child: Container(
                 decoration: BoxDecoration(
                   color: Colors.grey[100],
@@ -123,25 +136,35 @@ class _AppSearchBarState extends State<AppSearchBar> {
                 ),
               ),
               const PopupMenuItem<SearchType>(
-                value: SearchType.user,
-                child: Row(
-                  children: [
-                    Icon(Icons.person),
-                    SizedBox(width: 4.0),
-                    Text("User"),
-                  ],
-                ),
-              ),
-              const PopupMenuItem<SearchType>(
                 value: SearchType.author,
                 child: Row(
                   children: [
-                    Icon(Icons.science_rounded),
+                    Icon(Icons.person),
                     SizedBox(width: 4.0),
                     Text("Author"),
                   ],
                 ),
               ),
+              const PopupMenuItem<SearchType>(
+                value: SearchType.by,
+                child: Row(
+                  children: [
+                    Icon(Icons.person_2_outlined),
+                    SizedBox(width: 4.0),
+                    Text("By"),
+                  ],
+                ),
+              ),
+              const PopupMenuItem<SearchType>(
+                value: SearchType.both,
+                child: Row(
+                  children: [
+                    Icon(Icons.list_rounded),
+                    SizedBox(width: 4.0),
+                    Text("Both"),
+                  ],
+                ),
+              )
             ],
             child: searchTypeSelector(),
           ),
