8d6175ef28d745113d373a40c567318d63087479
Leyla Yayladere <leylayayladere@Leylas-MBP.home>
2023-10-30 02:59:49 +0300

    update profile page with activities tab

--- project/FrontEnd/collaborative_science_platform/lib/screens/profile_page/profile_page.dart
+++ project/FrontEnd/collaborative_science_platform/lib/screens/profile_page/profile_page.dart
@@ -2,9 +2,15 @@ import 'package:collaborative_science_platform/models/profile_data.dart';
 import 'package:collaborative_science_platform/models/user.dart';
 import 'package:collaborative_science_platform/providers/auth.dart';
 import 'package:collaborative_science_platform/providers/profile_data_provider.dart';
+import 'package:collaborative_science_platform/screens/home_page/home_page_appbar.dart';
+import 'package:collaborative_science_platform/screens/page_with_appbar.dart';
 import 'package:collaborative_science_platform/screens/profile_page/widgets/about_me.dart';
-import 'package:collaborative_science_platform/screens/profile_page/account_settings.dart';
+import 'package:collaborative_science_platform/screens/profile_page/widgets/desktop_edit_profile.dart';
+import 'package:collaborative_science_platform/screens/profile_page/widgets/mobile_edit_profile.dart';
+import 'package:collaborative_science_platform/screens/profile_page/widgets/profile_activity_tabbar.dart';
+import 'package:collaborative_science_platform/utils/textStyles.dart';
 import 'package:collaborative_science_platform/utils/responsive/responsive.dart';
+import 'package:collaborative_science_platform/widgets/card_container.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -17,10 +23,9 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-// TODO: add tabbar, PageWithTabBar() gives error, check it out
-// TODO: connect NodeTab and QuestionTab to profileData
-// TODO: add optional parameter to ProfilePage to show others profile page
-// TODO: fix overflow on mobile for About Me section
+// TODO: connect NodeActivity and QuestionActivity to profileData
+// TODO: add optional parameter to ProfilePage to get others profileData
+// TODO: make NodeTab clickable which directs to NodeDetailsPage
 class _ProfilePageState extends State<ProfilePage> {
   ProfileData profileData = ProfileData();
   int noWorks = 0;
@@ -29,6 +34,15 @@ class _ProfilePageState extends State<ProfilePage> {
   bool isLoading = false;
 
   bool _isFirstTime = true;
+
+  int currentIndex = 0;
+
+  void updateIndex(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+    print(currentIndex);
+  }
 
   @override
   void didChangeDependencies() {
@@ -64,103 +78,16 @@ class _ProfilePageState extends State<ProfilePage> {
     final User? user = Provider.of<Auth>(context).user;
     if (user == null) {
       // guest can see profile pages
-    } else if (user.email == "omer.unal@boun.edu.tr") {
+    } else if (user.email == profileData.email) {
       // own profile page, should be editible
-      return DefaultTabController(
-        length: 2, // Number of tabs
+      return PageWithAppBar(
+        appBar: const HomePageAppBar(),
+        pageColor: Colors.grey.shade200,
         child: Responsive(
-          desktop: NestedScrollView(
-            headerSliverBuilder: (context, innerBoxIsScrolled) => [
-              SliverToBoxAdapter(
-                child: Container(
-                  padding: const EdgeInsets.all(12.0),
-                  color: Colors.white,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      AboutMe(
-                        aboutMe: profileData.aboutMe,
-                        email: profileData.email,
-                        name: profileData.name,
-                        surname: profileData.surname,
-                        noWorks: noWorks,
-                      ),
-                      MyPadding(),
-                      EditProfileDesktop(),
-                    ],
-                  ),
-                ),
-              ),
-              SliverPersistentHeader(
-                pinned: true,
-                delegate: ProfileTabbar(
-                  child: Container(),
-                ),
-              ),
-            ],
-            body: TabBarView(
-              children: [
-                NodeTab(),
-                QuestionTab(),
-              ],
-            ),
-          ),
-          mobile: NestedScrollView(
-            headerSliverBuilder: (context, innerBoxIsScrolled) => [
-              SliverToBoxAdapter(
-                child: Container(
-                  padding: const EdgeInsets.all(12.0),
-                  color: Colors.white,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      AboutMe(
-                        aboutMe: profileData.aboutMe,
-                        email: profileData.email,
-                        name: profileData.name,
-                        surname: profileData.surname,
-                        noWorks: noWorks,
-                      ),
-                      MyPadding(),
-                      EditProfileMobile(),
-                      MyPadding(),
-                      LogOutMobile()
-                    ],
-                  ),
-                ),
-              ),
-              SliverPersistentHeader(
-                pinned: true,
-                delegate: ProfileTabbar(
-                  child: Container(),
-                ),
-              ),
-            ],
-            body: TabBarView(
-              children: [
-                NodeTab(),
-                QuestionTab(),
-              ],
-            ),
-          ),
-        ),
-      );
-    }
-
-    // others profile page, will be same both on desktop and mobile
-    return DefaultTabController(
-      length: 2, // Number of tabs
-      child: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          SliverToBoxAdapter(
-            child: Container(
-              padding: const EdgeInsets.all(12.0),
-              color: Colors.white,
+          mobile: SingleChildScrollView(
+            child: SizedBox(
+              width: Responsive.getGenericPageWidth(context),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   AboutMe(
                     aboutMe: profileData.aboutMe,
@@ -169,69 +96,149 @@ class _ProfilePageState extends State<ProfilePage> {
                     surname: profileData.surname,
                     noWorks: noWorks,
                   ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    child: MobileEditProfile(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    child: MobileLogOut(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    child: ProfileActivityTabBar(
+                      callback: updateIndex,
+                    ),
+                  ),
+                  if (currentIndex == 0)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      child: CardContainer(
+                        child: Container(
+                          height: 400,
+                          child: NodeActivity(),
+                        ),
+                      ),
+                    ),
+                  if (currentIndex == 1)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      child: CardContainer(
+                        child: Container(
+                          height: 400,
+                          child: QuestionActivity(),
+                        ),
+                      ),
+                    ),
                 ],
               ),
             ),
           ),
-          SliverPersistentHeader(
-            pinned: true,
-            delegate: ProfileTabbar(
-              child: Container(),
-            ),
-          ),
-        ],
-        body: TabBarView(
-          children: [
-            NodeTab(),
-            QuestionTab(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class EditProfileDesktop extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: GestureDetector(
-        onTap: () {
-          // Show Popup with EditProfileForm content
-          showDialog(
-              context: context,
-              builder: (context) => const AlertDialog(
-                    title: SizedBox(
-                      width: 500,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Edit Profile',
-                              style: TextStyle(fontSize: 20.0)),
-                        ],
+          desktop: SingleChildScrollView(
+            child: SizedBox(
+              width: Responsive.getGenericPageWidth(context),
+              child: Column(
+                children: [
+                  AboutMe(
+                    aboutMe: profileData.aboutMe,
+                    email: profileData.email,
+                    name: profileData.name,
+                    surname: profileData.surname,
+                    noWorks: noWorks,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    child: DesktopEditProfile(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    child: ProfileActivityTabBar(
+                      callback: updateIndex,
+                    ),
+                  ),
+                  if (currentIndex == 0)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      child: CardContainer(
+                        child: Container(
+                          height: 400,
+                          child: NodeActivity(),
+                        ),
                       ),
                     ),
-                    backgroundColor: Colors.white,
-                    shadowColor: Colors.white,
-                    content: EditProfileForm(),
-                  ));
-        },
-        child: Container(
-          height: 40.0,
-          width: MediaQuery.of(context).size.width - 80,
-          decoration: BoxDecoration(
-              color: Colors.blue, borderRadius: BorderRadius.circular(5.0)),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+                  if (currentIndex == 1)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      child: CardContainer(
+                        child: Container(
+                          height: 400,
+                          child: QuestionActivity(),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    // others profile page, will be same both on desktop and mobile
+    return PageWithAppBar(
+      appBar: const HomePageAppBar(),
+      pageColor: Colors.grey.shade200,
+      child: SingleChildScrollView(
+        child: SizedBox(
+          width: Responsive.getGenericPageWidth(context),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Icon(Icons.edit, color: Colors.white),
-              SizedBox(width: 10.0),
-              Text('Edit Profile',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0)),
+              AboutMe(
+                aboutMe: profileData.aboutMe,
+                email: profileData.email,
+                name: profileData.name,
+                surname: profileData.surname,
+                noWorks: noWorks,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: ProfileActivityTabBar(
+                  callback: updateIndex,
+                ),
+              ),
+              if (currentIndex == 0)
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: CardContainer(
+                    child: Container(
+                      height: 400,
+                      child: NodeActivity(),
+                    ),
+                  ),
+                ),
+              if (currentIndex == 1)
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: CardContainer(
+                    child: Container(
+                      height: 400,
+                      child: QuestionActivity(),
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
@@ -240,39 +247,7 @@ class EditProfileDesktop extends StatelessWidget {
   }
 }
 
-class EditProfileMobile extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: GestureDetector(
-        onTap: () {
-          Navigator.pushNamed(context, AccountSettingsPage.routeName);
-        },
-        child: Container(
-          height: 40.0,
-          width: MediaQuery.of(context).size.width - 80,
-          decoration: BoxDecoration(
-              color: Colors.blue, borderRadius: BorderRadius.circular(5.0)),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.edit, color: Colors.white),
-              SizedBox(width: 10.0),
-              Text('Edit Profile',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0)),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class LogOutMobile extends StatelessWidget {
+class MobileLogOut extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
@@ -300,54 +275,7 @@ class LogOutMobile extends StatelessWidget {
   }
 }
 
-class ProfileTabbar extends SliverPersistentHeaderDelegate {
-  final Widget child;
-
-  ProfileTabbar({required this.child});
-
-  @override
-  Widget build(
-    BuildContext context,
-    double shrinkOffset,
-    bool overlapsContent,
-  ) {
-    return Material(
-      color: Colors.white,
-      child: TabBar(
-        labelColor: Colors.black,
-        indicatorColor: Colors.black,
-        tabs: [
-          Tab(
-            icon: Icon(
-              Icons.account_tree_rounded, // represents Node
-              color: Colors.black,
-            ),
-          ),
-          Tab(
-            icon: Icon(
-              Icons.question_answer_outlined,
-              color: Colors.black,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  // TODO: implement maxExtent
-  double get maxExtent => 48;
-
-  @override
-  // TODO: implement minExtent
-  double get minExtent => 48;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
-      false;
-}
-
-class NodeTab extends StatelessWidget {
+class NodeActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -355,7 +283,8 @@ class NodeTab extends StatelessWidget {
         itemCount: 10, // Replace with the desired number of items
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
-            title: Text('Nodes Tab Content $index'),
+            title:
+                Text('Nodes Tab Content $index', style: TextStyles.bodyBlack),
           );
         },
       ),
@@ -363,7 +292,7 @@ class NodeTab extends StatelessWidget {
   }
 }
 
-class QuestionTab extends StatelessWidget {
+class QuestionActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -371,20 +300,11 @@ class QuestionTab extends StatelessWidget {
         itemCount: 10, // Replace with the desired number of items
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
-            title: Text('Questions Tab Content $index'),
+            title: Text('Questions Tab Content $index',
+                style: TextStyles.bodyBlack),
           );
         },
       ),
-    );
-  }
-}
-
-class MyPadding extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
-      child: Divider(height: 40.0),
     );
   }
 }
