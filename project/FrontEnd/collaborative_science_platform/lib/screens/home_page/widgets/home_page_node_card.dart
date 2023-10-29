52f4dad9940f80a93a3f297a6ac2732bca528cd6
Omer <omerfaunal@outlook.com>
2023-10-29 12:24:17 +0300

    Color and padding changes and add InkWell

--- project/FrontEnd/collaborative_science_platform/lib/screens/home_page/widgets/home_page_node_card.dart
+++ project/FrontEnd/collaborative_science_platform/lib/screens/home_page/widgets/home_page_node_card.dart
@@ -1,78 +1,76 @@
-
 import 'package:collaborative_science_platform/helpers/date_to_string.dart';
 import 'package:collaborative_science_platform/models/small_node.dart';
 import 'package:flutter/material.dart';
 
 class HomePageNodeCard extends StatelessWidget {
   final SmallNode smallNode;
-  final Color color;
+  final Color? color;
   final Function() onTap;
 
   const HomePageNodeCard({
     super.key,
     required this.smallNode,
-    required this.color,
+    this.color,
     required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: MouseRegion(
-        cursor: SystemMouseCursors.click,
-        child: GestureDetector(
-          onTap: onTap, // Navigate to the Screen of the Node
-          child: Card(
-            color: color,
-            elevation: 4.0,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Card(
+      elevation: 4.0,
+      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: InkWell(
+        onTap: onTap, // Navigate to the screen of the Node
+        customBorder: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                smallNode.nodeTitle,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                smallNode.contributors.join(", "),
+                style: const TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14.0,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Row(
-                    children: [
-                      Flexible(
-                        child: Text(
-                          smallNode.nodeTitle,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16.0,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 10.0),
-                      Text(
-                        getDurationFromNow(smallNode.publishDate),
-                        style: TextStyle(
-                          color: Colors.grey.shade600,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
                   Text(
-                    smallNode.contributors.join(", "),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 14.0,
-                      color: Colors.grey.shade600,
+                    getDurationFromNow(smallNode.publishDate),
+                    style: const TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w600,
                     ),
-                  ),
-                  Text(
-                    smallNode.theorem,
-                    maxLines: 4,
-                    overflow: TextOverflow.ellipsis,
                   ),
                 ],
               ),
-            ),
+              // SizedBox(height: 12.0),
+              // Text(
+              //   smallNode.theorem,
+              //   maxLines: 4,
+              //   overflow: TextOverflow.ellipsis,
+              //   style: TextStyle(
+              //     fontSize: 14.0,
+              //   ),
+              // ),
+            ],
           ),
         ),
       ),
