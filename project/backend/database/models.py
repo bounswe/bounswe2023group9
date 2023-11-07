a5d1c2e5536a64fc07cbfa4aef2470c1495d360d
Ali Mert Geben <gebenalimert@gmail.com>
2023-11-07 21:15:35 +0300

    Added User class to Admin

--- project/backend/database/models.py
+++ project/backend/database/models.py
@@ -11,6 +11,7 @@ class Workspace(models.Model):
      in order to be referred from other classes. e.g. Contributor
     """
     pass
+
 class Entry(models.Model):
     """
      This class definition is written beforehand (to be implemented afterwards)
@@ -36,7 +37,7 @@ class Contributor(BasicUser):
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
-    
+
     """
      Methods below (create/delete Workspace instances) should be reinvestigated 
      after implementation of Workspace class.
@@ -45,17 +46,17 @@ class Contributor(BasicUser):
         new_workspace = Workspace.objects.create()
         self. workspaces.add(new_workspace)
         return new_workspace
-      
+
     def delete_workspace(self, workspace_to_delete):        # Note that this function doesn't delete the
-        if workspace_to_delete in self.workspaces.all():    # Workspace but pops from the list to prevent 
+        if workspace_to_delete in self.workspaces.all():    # Workspace but pops from the list to prevent
             self.workspaces.remove(workspace_to_delete)     # errors if multiple Contributors present
-                                            
+
 class Reviewer(Contributor):
-    
+
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
-    
-    def get_review_requests(self):                          
+
+    def get_review_requests(self):
         return ReviewRequest.objects.filter(reviewer=self)
 
 
@@ -152,25 +153,24 @@ class Node(models.Model):
     def increment_num_visits(self):
         self.num_visits += 1
 
-class Workspace:
+class Workspace(models.Model):
     workspace_id = models.AutoField(primary_key=True)
-    node = models.ForeignKey(Node, on_delete=models.SET_NULL, related_name="Workspaces") #?
+    node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name="Workspaces") #?
     workspace_title = models.CharField(max_length=100)
-    contributors = models.ManyToManyField(Contributor, related_name='WorkspaceContributors')
-    collab_requests = models.ManyToManyField(Request, related_name='CollaborationRequests')
-    references = models.ManyToManyField(Node, related_name = 'WorkspaceReferences')
-    reviews = models.ManyToManyField(ReviewRequest,related_name = 'WorkspaceReviews') #?
-    semantic_tags = models.ManyToManyField(SemanticTagrelated_name = 'WorkspaceSemanticTags')
-    wiki_tags = models.ManyToManyField(WikiTag,related_name = 'WorkspaceWikiTags')
+    contributors = models.ForeignKey(Contributor, on_delete=models.PROTECT, related_name='WorkspaceContributors')
+    collab_requests = models.ForeignKey(Request, on_delete=models.PROTECT,null=True, blank=True,related_name='CollaborationRequests')
+    references = models.ForeignKey(Node, on_delete=models.PROTECT,related_name = 'WorkspaceReferences')
+    reviews = models.ForeignKey(ReviewRequest,on_delete=models.PROTECT,null=True, blank=True,related_name = 'WorkspaceReviews') #?
+    semantic_tags = models.ForeignKey(SemanticTag,on_delete=models.PROTECT,null=True, blank=True,related_name = 'WorkspaceSemanticTags')
+    wiki_tags = models.ForeignKey(WikiTag,on_delete=models.PROTECT,null=True, blank=True,related_name = 'WorkspaceWikiTags')
     is_finalized = models.BooleanField()
     is_published = models.BooleanField()
     is_in_review = models.BooleanField()
     is_rejected = models.BooleanField()
-    theorem_posted = models.BooleanField() 
+    theorem_posted = models.BooleanField()
     num_approvals = models.IntegerField()
-    theorem_entry = models.ForeignKey(Entry,on_delete=models.SET_NULL)
-    final_entry = models.ForeignKey(Entry,on_delete=models.SET_NULL)
-    entries = models.ManyToManyField(Entry, related_name='WorkspaceEntries')
+    theorem_entry = models.ForeignKey(Entry,on_delete=models.CASCADE,related_name='TheoremEntry')
+    final_entry = models.ForeignKey(Entry,on_delete=models.CASCADE,related_name='FinalEntry')
     def finalize_workspace(self):
         self.is_finalized = True
         self.is_in_review = False
