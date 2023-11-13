6bba2f00a9a4cf97692e8613aaa8f361de90ce28
brunettow <bengisutakkin34@hotmail.com>
2023-11-13 21:15:16 +0300

    changed page design

--- project/FrontEnd/collaborative_science_platform/lib/screens/graph_page/web_graph_page.dart
+++ project/FrontEnd/collaborative_science_platform/lib/screens/graph_page/web_graph_page.dart
@@ -3,6 +3,7 @@ import 'package:collaborative_science_platform/models/small_node.dart';
 import 'package:collaborative_science_platform/screens/graph_page/widgets/center_node.dart';
 import 'package:collaborative_science_platform/screens/graph_page/widgets/node_list.dart';
 import 'package:collaborative_science_platform/screens/home_page/widgets/home_page_appbar.dart';
+import 'package:collaborative_science_platform/screens/home_page/widgets/home_page_node_card.dart';
 import 'package:collaborative_science_platform/screens/page_with_appbar/page_with_appbar.dart';
 import 'package:collaborative_science_platform/utils/responsive/responsive.dart';
 import 'package:flutter/material.dart';
@@ -20,18 +21,81 @@ class WebGraphPage extends StatefulWidget {
 }
 
 class _WebGraphPageState extends State<WebGraphPage> {
-  SmallNode node = SmallNode(
-      nodeId: 1,
-      nodeTitle: "Altkümelerin Üretici Fonksiyonları",
-      contributors: [Contributor(name: "Abdullah", surname: "Susuz", email: "demo@boun.edu.tr")],
-      publishDate: DateTime(2023));
+  List<SmallNode> references = [];
+  List<SmallNode> citations = [];
+  bool areReferencesLoading = false;
+  bool areCitationsLoading = false;
+
+  void getReferences() {
+    setState(() {
+      areReferencesLoading = true;
+    });
+    references = List<SmallNode>.generate(
+      10,
+      (index) => SmallNode(
+        nodeId: index + 1,
+        nodeTitle: "Reference ${index + 1}",
+        contributors: [
+          Contributor(
+              name: "Contributor Name ${index + 1}",
+              surname: "Contributor Surname ${index + 1}",
+              email: "contributor${index + 1}@mail.com"),
+        ],
+        publishDate: DateTime(1590, 12, 12),
+      ),
+    );
+    setState(() {
+      areReferencesLoading = false;
+    });
+  }
+
+  void getCitations() {
+    setState(() {
+      areCitationsLoading = true;
+    });
+    citations = List<SmallNode>.generate(
+      10,
+      (index) => SmallNode(
+        nodeId: index + 1,
+        nodeTitle: "Citation ${index + 1}",
+        contributors: [
+          Contributor(
+              name: "Contributor Name ${index + 1}",
+              surname: "Contributor Surname ${index + 1}",
+              email: "contributor${index + 1}@mail.com"),
+        ],
+        publishDate: DateTime(1990, 12, 12),
+      ),
+    );
+    setState(() {
+      areCitationsLoading = false;
+    });
+  }
+
+  Widget referencesCardList() {
+    getReferences();
+    return ListView.builder(
+      itemCount: references.length,
+      itemBuilder: (context, index) => HomePageNodeCard(
+        smallNode: references[index],
+        onTap: () {/* Orientate the node in the middle */},
+      ),
+    );
+  }
+
+  Widget citationsCardList() {
+    getCitations();
+    return ListView.builder(
+      itemCount: citations.length,
+      itemBuilder: (context, index) => HomePageNodeCard(
+        smallNode: citations[index],
+        onTap: () {/* Orientate the node to the middle */},
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    // others profile page, will be the same both on desktop and mobile
-    List<SmallNode> references = List.generate(15, (index) => node);
-    List<SmallNode> citations = List.generate(15, (index) => node);
-
     const listRatio = 0.2;
 
     return PageWithAppBar(
@@ -39,30 +103,33 @@ class _WebGraphPageState extends State<WebGraphPage> {
       pageColor: Colors.grey.shade200,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Flexible(
-            flex: 2,
-            child: NodeList(
-              nodes: references,
-              title: "References",
-              width: Responsive.getGenericPageWidth(context) * listRatio,
+          Container(
+            width: MediaQuery.of(context).size.width / 3.2,
+            height: MediaQuery.of(context).size.height,
+            child: referencesCardList(),
+          ),
+          Container(
+               width: MediaQuery.of(context).size.width / 3,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                SizedBox(height: 30.0),
+                Flexible(
+                  flex: 6,
+                  child: CenterNode(
+                    node: widget.smallNode,
+                    width: Responsive.getGenericPageWidth(context) * (1 - listRatio * 2),
+                  ),
+                ),
+              ],
             ),
           ),
-          Flexible(
-            flex: 6,
-            child: CenterNode(
-              node: node,
-              width: Responsive.getGenericPageWidth(context) * (1 - listRatio * 2),
-            ),
-          ),
-          Flexible(
-            flex: 2,
-            child: NodeList(
-              nodes: citations,
-              title: "Citations",
-              width: Responsive.getGenericPageWidth(context) * listRatio,
-            ),
+          Container(
+            width: MediaQuery.of(context).size.width / 3.2,
+            height: MediaQuery.of(context).size.height,
+            child: citationsCardList(),
           ),
         ],
       ),
