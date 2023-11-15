95d38b278855fc907e7ddb7258e7a4ec4482a785
Ali Mert Geben <79109830+gebenalimert@users.noreply.github.com>
2023-11-15 23:47:02 +0300

    Update models.py

--- project/backend/database/models.py
+++ project/backend/database/models.py
@@ -57,9 +57,9 @@ class Entry(models.Model):
     is_theorem_entry = models.BooleanField()
     is_final_entry = models.BooleanField(default=False)
     is_editable = models.BooleanField(default=True)
-    creator = models.ForeignKey(Contributor,null=True,blank=True, on_delete = models.CASCADE)
+    #creator = models.ForeignKey(Contributor,null=True,blank=True, on_delete = models.CASCADE)
     entry_number = models.IntegerField()
-    contributors = models.ManyToManyField(Contributor,related_name="EntryContributors")
+    #contributors = models.ManyToManyField(Contributor,related_name="EntryContributors")
     def set_as_final(self):
         self.is_final_entry = True
     def set_as_theorem(self):
