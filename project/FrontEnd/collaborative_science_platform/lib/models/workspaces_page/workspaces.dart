4c1eb52657b1201ed4d831b4f610a36b4fc66a7f
Zulalm <zulalmolla@gmail.com>
2023-11-18 20:39:09 +0300

    workspace models are created

---
+++ project/FrontEnd/collaborative_science_platform/lib/models/workspaces_page/workspaces.dart
@@ -0,0 +1,22 @@
+import 'package:collaborative_science_platform/models/workspaces_page/workspaces_object.dart';
+
+class Workspaces {
+  List<WorkspacesObject> workspaces;
+  List<WorkspacesObject> pendingWorkspaces;
+  Workspaces({
+    required this.workspaces,
+    required this.pendingWorkspaces,
+  });
+
+  factory Workspaces.fromJson(Map<String, dynamic> jsonString) {
+    var workspacesList = jsonString['workspaces'] as List;
+    var pendingWorkspacesList = jsonString['workspaces'] as List;
+
+    List<WorkspacesObject> workspaces =
+        workspacesList.map((e) => WorkspacesObject.fromJson(e)).toList();
+
+    List<WorkspacesObject> pendingWorkspaces =
+        pendingWorkspacesList.map((e) => WorkspacesObject.fromJson(e)).toList();
+    return Workspaces(workspaces: workspaces, pendingWorkspaces: pendingWorkspaces);
+  }
+}
