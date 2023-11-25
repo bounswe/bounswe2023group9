b4e093e12ee04291b517d3942bfd1c1455e2b551
Zulalm <zulalmolla@gmail.com>
2023-11-25 23:02:32 +0300

    provider is implemented for workspace

--- project/FrontEnd/collaborative_science_platform/lib/screens/workspace_page/workspaces_page.dart
+++ project/FrontEnd/collaborative_science_platform/lib/screens/workspace_page/workspaces_page.dart
@@ -1,18 +1,114 @@
+import 'package:collaborative_science_platform/exceptions/workspace_exceptions.dart';
+import 'package:collaborative_science_platform/models/workspaces_page/workspace.dart';
+import 'package:collaborative_science_platform/models/workspaces_page/workspaces.dart';
+import 'package:collaborative_science_platform/providers/auth.dart';
+import 'package:collaborative_science_platform/providers/workspace_provider.dart';
 import 'package:collaborative_science_platform/screens/workspace_page/web_workspace_page/web_workspace_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../utils/responsive/responsive.dart';
 import 'mobile_workspace_page/mobile_workspace_page.dart';
 
-class WorkspacesPage extends StatelessWidget {
+class WorkspacesPage extends StatefulWidget {
   static const routeName = '/workspaces';
-  const WorkspacesPage({super.key});
+  final int workspaceId;
+  const WorkspacesPage({super.key, this.workspaceId = -1});
+  @override
+  State<WorkspacesPage> createState() => _WorkspacesPageState();
+}
+
+class _WorkspacesPageState extends State<WorkspacesPage> {
+  bool _isFirstTime = true;
+  bool error = false;
+  String errorMessage = "";
+
+  bool isLoading = false;
+
+  Workspace? workspace;
+  Workspaces? workspaces;
+
+  void getWorkspaceById(int id) async {
+    try {
+      final workspaceProvider = Provider.of<WorkspaceProvider>(context);
+      setState(() {
+        error = false;
+        isLoading = true;
+      });
+      await workspaceProvider.getWorkspaceById(id);
+      setState(() {
+        workspace = (workspaceProvider.workspace ?? {} as Workspace);
+      });
+    } on WorkspaceDoesNotExist {
+      setState(() {
+        error = true;
+        errorMessage = WorkspaceDoesNotExist().message;
+      });
+    } catch (e) {
+      setState(() {
+        error = true;
+        errorMessage = "Something went wrong!";
+      });
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  void getUserWorkspaces(int id) async {
+    try {
+      final auth = Provider.of<Auth>(context);
+      final workspaceProvider = Provider.of<WorkspaceProvider>(context);
+      setState(() {
+        error = false;
+        isLoading = true;
+      });
+      await workspaceProvider.getUserWorkspaces(auth.user!.id);
+      setState(() {
+        workspaces = (workspaceProvider.workspaces ?? {} as Workspaces);
+      });
+    } on WorkspaceDoesNotExist {
+      setState(() {
+        error = true;
+        errorMessage = WorkspaceDoesNotExist().message;
+      });
+    } catch (e) {
+      setState(() {
+        error = true;
+        errorMessage = "Something went wrong!";
+      });
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isFirstTime) {
+      if (widget.workspaceId > -1) {
+        getWorkspaceById(widget.workspaceId);
+      }
+      getUserWorkspaces(41);
+
+      _isFirstTime = false;
+    }
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Responsive(
-      mobile: MobileWorkspacePage(),
-      desktop: WebWorkspacePage(),
+    return Responsive(
+      mobile: MobileWorkspacePage(
+        workspace: workspace,
+        workspaces: workspaces,
+      ),
+      desktop: WebWorkspacePage(
+        workspace: workspace,
+        workspaces: workspaces,
+      ),
     );
   }
 }
