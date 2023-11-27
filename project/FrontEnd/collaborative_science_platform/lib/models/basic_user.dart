38eb36a90d23a0b0784b0c68f3d558286ec465d9
Zulalm <zulalmolla@gmail.com>
2023-11-27 14:58:21 +0300

    basic user model is created

--- project/FrontEnd/collaborative_science_platform/lib/models/basic_user.dart
+++ project/FrontEnd/collaborative_science_platform/lib/models/basic_user.dart
@@ -1,25 +1,22 @@
-import 'package:collaborative_science_platform/models/account.dart';
-import 'package:collaborative_science_platform/models/annotation.dart';
-import 'package:collaborative_science_platform/models/base_user.dart';
-import 'package:collaborative_science_platform/models/notification.dart';
-
-class BasicUser extends BaseUser {
-  int userId;
-  Account account;
-  List<Notification> notifications;
-  List<Annotation> annotations;
+class BasicUser {
+  int basicUserId;
+  String bio;
+  bool emailNotificationPreference;
+  bool showActivity;
 
   BasicUser({
-    required int sessionID,
-    required DateTime logDate,
-    required bool privacyPoliciesAccepted,
-    required this.userId,
-    required this.account,
-    required this.notifications,
-    required this.annotations,
-  }) : super(
-          sessionID: sessionID,
-          logDate: logDate,
-          privacyPoliciesAccepted: privacyPoliciesAccepted,
-        );
+    required this.basicUserId,
+    required this.bio,
+    required this.emailNotificationPreference,
+    required this.showActivity,
+  });
+  factory BasicUser.fromJson(Map<String, dynamic> jsonString) {
+    return BasicUser(
+      basicUserId: jsonString["basic_user_id"],
+      bio: jsonString["bio"],
+      emailNotificationPreference: jsonString["email_notification_preference"],
+      showActivity: jsonString["show_activity_preference"],
+    );
+  }
+
 }
