9e9fc357f5598f68513574253a9ec6eb6eefbc4e
Ahmed Bera Pay <abera.pay@gmail.com>
2023-12-08 11:49:55 +0300

    Implement Admin API
    Admin API to hide/unhide or ban/unban is implemented.
    Also missing teardown functions are added to test classes to fix errors.
    Question model is updated to include removed_by_admin boolean field.

--- project/backend/api/urls.py
+++ project/backend/api/urls.py
@@ -36,4 +36,5 @@ urlpatterns = [
     path('get_semantic_suggestion/', get_semantic_suggestion, name='get_semantic_suggestion'),
     path('ask_question/', AskQuestion.as_view(), name='ask_question'),
     path('answer_question/', AnswerQuestion.as_view(), name='answer_question'),
+    path('update_content_status/', update_content_status, name='update_content_status'),
 ]
