09f4631143d2a2d3c39b8c39aefba4509a1ab50b
Simurgan <uyduranomersukru@gmail.com>
2023-12-15 20:59:20 +0300

    remove forgotten django rest framework methods and classes

--- project/annotation_project/annotations/admin.py
+++ project/annotation_project/annotations/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.register(Source)
+admin.site.register(Selector)
+admin.site.register(Body)
+admin.site.register(Creator)
 admin.site.register(Annotation)
--- project/annotation_project/annotations/views.py
+++ project/annotation_project/annotations/views.py
@@ -1,16 +1,3 @@
 from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
 
 from .models import *
-from .serializers import *
-
-@api_view(['GET'])
-def get_annotation(request):
-    id = request.GET.get('id')
-    if id is None:
-        for annotation in Annotation.objects.all():
-            return Response(AnnotationSerializer(annotation).data)
-        
-    return Response({"message": "No content"}, status=status.HTTP_200_OK)
