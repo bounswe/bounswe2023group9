60808a18cdc16682eb73b7b81294d0a04eebe8b2
Ahmed Bera Pay <abera.pay@gmail.com>
2023-12-16 08:43:52 +0300

    Implement GET API
    Implemented GET methods to
    - return matched annotations according to source URI and creator email
    - return single annotation given its ID

--- project/annotation_project/annotations/urls.py
+++ project/annotation_project/annotations/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from .views  import *
 
 urlpatterns = [
+    path('get_annotation/', matched_annotations_get_view, name='get_annotation'),
+    path('annotation<annotation_id>/', get_annotation_by_id, name='get_annotation_by_id'),
+
 ]
 
 
--- project/annotation_project/annotations/views.py
+++ project/annotation_project/annotations/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from .models import *
+
+
+def serialize_annotation(annotation, host):
+    return {
+        '@context': 'http://www.w3.org/ns/anno.jsonld',
+        'id': f'http://{host}/annotation{annotation.id}',
+        'type': annotation.type,
+        'body': {
+            'type': annotation.body.type,
+            'format': annotation.body.format,
+            'language': annotation.body.language,
+            'value': annotation.body.value,
+        },
+        'target': {
+            'id': annotation.target.source.uri,
+            'type': 'text',
+            'selector': {
+                'type': annotation.target.type,
+                'start': annotation.target.start,
+                'end': annotation.target.end,
+            },
+        },
+        'creator': {
+            'id': f'http://13.51.205.39/profile/{annotation.creator.name}',
+            'type': annotation.creator.type,
+        },
+        'created': annotation.created,
+    }
+
+
+def matched_annotations_get_view(request):
+    source_uri = request.GET.get('source')
+    creator_name = request.GET.get('creator')
+
+    filtering_conditions = {}
+
+    if source_uri:
+        filtering_conditions['target__source__uri__iexact'] = source_uri
+    if creator_name:
+        filtering_conditions['creator__name__iexact'] = creator_name
+    try:
+        matched_annotations = Annotation.objects.filter(**filtering_conditions)
+
+        if len(matched_annotations) == 0:
+            return JsonResponse({'message': 'No annotation found!'}, status=404)
+
+        matched_data = [
+            serialize_annotation(annotation, request.get_host()) for annotation in matched_annotations
+        ]
+
+        return JsonResponse(matched_data, status=200, safe=False)
+    except Exception as e:
+        return JsonResponse({'message': str(e)}, status=500)
+
+
+def get_annotation_by_id(request, annotation_id):
+    try:
+        annotation = Annotation.objects.get(id=annotation_id)
+
+        if not annotation:
+            return JsonResponse({'message': 'No annotation found!'}, status=404)
+        return JsonResponse(data = serialize_annotation(annotation, request.get_host()), status=200)
+    except Exception as e:
+        return JsonResponse({'message': str(e)}, status=500)
