3be9e9bc3b32217fc03c68821a379a23217a38b1
laylaylo <74381463+laylaylo@users.noreply.github.com>
2023-12-23 20:58:10 +0200

    bug fix for showing multiple proofs

--- project/FrontEnd/collaborative_science_platform/lib/screens/node_details_page/widgets/proof_list_view.dart
+++ project/FrontEnd/collaborative_science_platform/lib/screens/node_details_page/widgets/proof_list_view.dart
@@ -11,6 +11,7 @@ import 'dart:convert';
 
 class ProofListView extends StatefulWidget {
   final List<Proof> proof;
+
   const ProofListView({super.key, required this.proof});
 
   @override
@@ -18,92 +19,122 @@ class ProofListView extends StatefulWidget {
 }
 
 class _ProofListViewState extends State<ProofListView> {
-  bool showAnnotations = false;
   @override
   Widget build(BuildContext context) {
     return Container(
       width: Responsive.desktopPageWidth,
       decoration: BoxDecoration(color: Colors.grey[200]),
-      child: ListView.builder(
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(8),
-          itemCount: widget.proof.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.all(5),
-              child: CardContainer(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Text(
-                    //   proof[index].isDisproof ? "Disproof" : "Proof",
-                    //   style: TextStyles.bodyGrey,
-                    //   textAlign: TextAlign.start,
-                    // ),
-                    // Text(
-                    //   proof[index].proofTitle,
-                    //   style: TextStyles.title4,
-                    //   textAlign: TextAlign.start,
-                    // ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        SizedBox(
-                            width: 180,
-                            child: AppButton(
-                                text: showAnnotations ? "Show Text" : "Show Annotations",
-                                height: 40,
-                                onTap: () {
-                                  setState(() {
-                                    showAnnotations = !showAnnotations;
-                                  });
-                                })),
-                      ],
-                    ),
-                    const SizedBox(height: 8),
-                    showAnnotations
-                        ? AnnotationText(
-                            utf8.decode(widget.proof[index].proofContent.codeUnits),
-                            annotationType: AnnotationType.proof,
-                          )
-                        : TeXView(
-                            renderingEngine: const TeXViewRenderingEngine.katex(),
-                            child: TeXViewDocument(
-                                utf8.decode(widget.proof[index].proofContent.codeUnits))),
+      child: SingleChildScrollView(
+        child: Column(
+          children: widget.proof.map((proof) => ProofItemWidget(proof: proof)).toList(),
+        ),
+      ),
+    );
+  }
+}
 
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.end,
-                    //   crossAxisAlignment: CrossAxisAlignment.end,
-                    //   children: [
-                    //     Icon(
-                    //       proof[index].isValid ? Icons.check : Icons.clear,
-                    //       color: proof[index].isValid ? AppColors.successColor : AppColors.dangerColor,
-                    //     ),
-                    //     Text(
-                    //       proof[index].isValid ? "valid" : "invalid",
-                    //       style: TextStyles.bodyGrey,
-                    //       textAlign: TextAlign.end,
-                    //     ),
-                    //   ],
-                    // ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          widget.proof[index].publishDate.toString(),
-                          style: TextStyles.bodyGrey,
-                          textAlign: TextAlign.end,
-                        )
-                      ],
-                    ),
-                  ],
+class ProofItemWidget extends StatefulWidget {
+  final Proof proof;
+
+  ProofItemWidget({Key? key, required this.proof}) : super(key: key);
+
+  @override
+  _ProofItemWidgetState createState() => _ProofItemWidgetState();
+}
+
+class _ProofItemWidgetState extends State<ProofItemWidget> {
+  bool showAnnotations = true;
+  bool isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Delay the rendering of TeXView
+    Future.delayed(Duration(milliseconds: 500), () {
+      if (mounted) {
+        setState(() {
+          isLoaded = true;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: CardContainer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Text(
+            //   proof[index].isDisproof ? "Disproof" : "Proof",
+            //   style: TextStyles.bodyGrey,
+            //   textAlign: TextAlign.start,
+            // ),
+            // Text(
+            //   proof[index].proofTitle,
+            //   style: TextStyles.title4,
+            //   textAlign: TextAlign.start,
+            // ),
+            // Conditional rendering based on isLoaded
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: 180,
+                  child: AppButton(
+                      text: showAnnotations ? "Show Text" : "Show Annotations",
+                      height: 40,
+                      onTap: () {
+                        setState(() {
+                          showAnnotations = !showAnnotations;
+                        });
+                      }),
                 ),
-              ),
-            );
-          }),
+              ],
+            ),
+            const SizedBox(height: 8),
+            showAnnotations
+                ? AnnotationText(
+                    utf8.decode(widget.proof.proofContent.codeUnits),
+                    annotationType: AnnotationType.proof,
+                  )
+                : isLoaded
+                    ? TeXView(
+                        renderingEngine: const TeXViewRenderingEngine.katex(),
+                        child: TeXViewDocument(utf8.decode(widget.proof.proofContent.codeUnits)))
+                    : SizedBox.shrink(),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.end,
+            //   crossAxisAlignment: CrossAxisAlignment.end,
+            //   children: [
+            //     Icon(
+            //       proof[index].isValid ? Icons.check : Icons.clear,
+            //       color: proof[index].isValid ? AppColors.successColor : AppColors.dangerColor,
+            //     ),
+            //     Text(
+            //       proof[index].isValid ? "valid" : "invalid",
+            //       style: TextStyles.bodyGrey,
+            //       textAlign: TextAlign.end,
+            //     ),
+            //   ],
+            // ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  widget.proof.publishDate.toString(),
+                  style: TextStyles.bodyGrey,
+                  textAlign: TextAlign.end,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
