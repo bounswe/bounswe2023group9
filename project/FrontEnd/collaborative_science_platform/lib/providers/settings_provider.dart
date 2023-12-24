dae0b5049cba9cbde6f120a73ef259698084dba6
brunettow <bengisutakkin34@hotmail.com>
2023-12-25 02:53:57 +0300

    bug fixed in settings

--- project/FrontEnd/collaborative_science_platform/lib/providers/settings_provider.dart
+++ project/FrontEnd/collaborative_science_platform/lib/providers/settings_provider.dart
@@ -5,27 +5,29 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class SettingsProvider with ChangeNotifier {
+
   Future<int> changePassword(User? user, String oldPass, String newPass) async {
     final Map<String, String> header = {
       "Accept": "application/json",
       "content-type": "application/json",
-      'Authorization': "Token"// ${user!.token}",
+      'Authorization': "Token ${user!.token}",
     };
-
+      final String body = json.encode({
+        'old_password': oldPass,
+        'password': newPass,
+          
+    });
+   
     try {
       final response = await http.put(
         Uri.parse("${Constants.apiUrl}/change_password/"),
         headers: header,
-        body: jsonEncode(
-          <String, String>{
-            'old_password': oldPass,
-            'new_password': newPass,
-          },
-        ),
+        body: body,
       );
       print(response.statusCode);
       return response.statusCode;
     } catch (e) {
+           print(e);
       rethrow;
     }
   }
@@ -34,23 +36,22 @@ class SettingsProvider with ChangeNotifier {
     final Map<String, String> header = {
       "Accept": "application/json",
       "content-type": "application/json",
-      'Authorization': "Token"// ${user!.token}",
+      'Authorization': "Token ${user!.token}",
     };
-
+      final String body = json.encode({
+        'bio': bio,
+        'email_notification_preference': sendNotification,
+        'show_activity_preference': showActivity
+          
+    });
     try {
       final response = await http.put(
         Uri.parse("${Constants.apiUrl}/change_profile_settings/"),
         headers: header,
-        body: jsonEncode(
-          <String, String>{
-            'bio': bio,
-            'email_notification_preference': sendNotification.toString(),
-            'show_activity_preference': showActivity.toString()
-          },
-        ),
+        body: body
       );
-      print(response.statusCode);
     } catch (e) {
+      print(e);
       rethrow;
     }
   }
